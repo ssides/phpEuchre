5598b97f93c7b2fe5f84a91e669913d48948be2b
ssides <steven.r.sides@gmail.com>
2022-08-31 13:15:33 -0400

    trying to register

--- header.php
+++ header.php
@@ -3,9 +3,8 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">PHP User Auth System</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
-            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">Sides Family Euchre</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"  aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
