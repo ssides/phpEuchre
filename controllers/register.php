cf5f071a84cf3122ff5049679bb290384cc107f0
ssides <steven.r.sides@gmail.com>
2022-08-31 13:56:41 -0400

    Update register.php

--- controllers/register.php
+++ controllers/register.php
@@ -24,7 +24,7 @@
                     User with email already exist!
                 </div>
             ';
-            header("Location: ../signup.php");
+            header("Location: register.php");
         }
         /*
 
