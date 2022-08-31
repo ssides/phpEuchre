137384bce7611a81bf6c633f8ad30a3ba5d12bc6
ssides <steven.r.sides@gmail.com>
2022-08-31 17:07:56 -0400

    Update login.php

--- controllers/login.php
+++ controllers/login.php
@@ -4,7 +4,19 @@
     include('config/db.php');
 
     if(isset($_POST['login'])) {
-      $sqlErr = "post";
+        $name_signin      = $_POST['name_signin'];
+        $password_signin  = $_POST['password_signin'];
+
+        $_name = mysqli_real_escape_string($connection, $name_signin);
+        $pswd = mysqli_real_escape_string($connection, $password_signin);
+        $sqlErr = "post";
+
+        // Query if email exists in db
+        $sql = "select * from `Players` where `Name` = '{$_name}' ";
+        $query = mysqli_query($connection, $sql);
+        $rowCount = mysqli_num_rows($query);
+
+      
     }
 
 ?>
