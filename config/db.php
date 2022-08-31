455066d32fe53be3172aaf751633578da80c0650
ssides <steven.r.sides@gmail.com>
2022-08-30 21:35:59 -0400

    trying a different repo

--- config/db.php
+++ config/db.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+
+    // Enable us to use Headers
+    ob_start();
+
+    // Set sessions
+    if(!isset($_SESSION)) {
+        session_start();
+    }
 
     global $hostname,$username,$password,$dbname
     $hostname = "localhost";
@@ -6,6 +14,6 @@
     $password = "3pTRrTjgLd6F1hnr";
     $dbname = "Euchre";
     
-    // $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.")
+    $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.")
 
 ?>
