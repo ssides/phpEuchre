db7c3397cf5beb19ace40e3f656aeea46fc296c4
ssides <steven.r.sides@gmail.com>
2022-09-01 09:51:47 -0400

    fixed index

--- controllers/setProfile.php
+++ controllers/setProfile.php
@@ -1,9 +1,9 @@
 <?php  
     if(isset($_POST['upload'])) {
       $dbg0 = count($_FILES['profileImage']);
-      $dbg1 = $_FILES['name'];
-      $dbg2 = $_FILES['type'];
-      $dbg3 = $_FILES['size'];
-      $dbg4 = $_FILES['error'];
+      $dbg1 = $_FILES['profileImage']['name'];
+      $dbg2 = $_FILES['profileImage']['type'];
+      $dbg3 = $_FILES['profileImage']['size'];
+      $dbg4 = $_FILES['profileImage']['error'];
     }
 ?>
