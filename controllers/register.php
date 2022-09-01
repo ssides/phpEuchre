08ff27ab4f6c96f448eeb1607212722018007630
ssides <steven.r.sides@gmail.com>
2022-09-01 09:16:15 -0400

    file upload research

--- controllers/register.php
+++ controllers/register.php
@@ -1,17 +1,7 @@
 <?php
    
-    // Database connection
     include('config/db.php');
-    
-    function GUID()
-    {
-        if (function_exists('com_create_guid') === true)
-        {
-            return trim(com_create_guid(), '{}');
-        }
-
-        return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
-    }
+    include('controllers/GUID.php');
     
     if(isset($_POST["submit"])) {
 
