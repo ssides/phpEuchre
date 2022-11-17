c67e7940689374ad278967a5b8e0a4e50fc72cd8
ssides <steven.r.sides@gmail.com>
2022-11-16 20:50:37 -0500

    Made progress toward dealing cards.
    Fixed a bug with dealing the first Jack, and put all that code together with the seven view models.

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
   // make sure there is a final path separator in paths.
-  $appUrl = 'http://localhost:8080/';
+  $appUrl = 'http://192.168.254.148:8080/';
   $cookieName = 'sidesLocalEuchre_cookie';
   $thumbnailDim = 25;
   $positionDistance = 3;
   // the images folder must be called images and it must be in root.
   $uploadsDir = 'C:/src/phpEuchre/images/';
-  $cardImageWidth = 71; // px
-  $cardImageHeight = 96; // px
+  $cardImageWidth = 33; // px 
+  $cardImageHeight = 45; // px
   $firstJackChoices = 500;
   $dealChoices = 1000;
 ?>
