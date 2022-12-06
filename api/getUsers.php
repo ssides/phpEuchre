f61c63f10ff75bfff137110e873a6411908e58c4
ssides <steven.r.sides@gmail.com>
2022-12-06 14:20:30 -0500

    Inactive users cannot be invited to join games.

--- api/getUsers.php
+++ api/getUsers.php
@@ -7,20 +7,28 @@
   if($_SERVER["REQUEST_METHOD"] === 'POST') {
     if (isset($_POST[$cookieName]) && isAuthenticated($_POST[$cookieName])) {
       
+      $response = array();
+      $response['ErrorMsg'] = "";
       $users = array();
       $sql = "select p.`ID`,substr(`Name`,1,8) `Name`,u.`ThumbnailPath`
               from `Player` p
               left join `UserProfile` u on u.`PlayerID` = p.`ID`
-              where p.`ID` <> '{$_POST[$cookieName]}'";
+              where p.`ID` <> '{$_POST[$cookieName]}' and p.`IsActive` = '1'";
+              
       $results = mysqli_query($connection, $sql);
-
-      while ($row = mysqli_fetch_array($results)) {
-        $tnURL = is_null($row['ThumbnailPath']) ? '' : getThumbnailURL($row['ThumbnailPath']);
-        array_push($users, array($row['ID'],$row['Name'],$tnURL));
+      if ($results === false) {
+        $response['ErrorMsg'] .= mysqli_error($connection);
+      } else {
+        while ($row = mysqli_fetch_array($results)) {
+          $tnURL = is_null($row['ThumbnailPath']) ? '' : getThumbnailURL($row['ThumbnailPath']);
+          array_push($users, array($row['ID'],$row['Name'],$tnURL));
+        }
       }
-
+      
+      $response['Users'] = $users;
+      
       http_response_code(200);
-      echo json_encode($users);
+      echo json_encode($response);
 
     } else {
       echo "ID invalid or missing.";
