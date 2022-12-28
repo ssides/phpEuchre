fda826b161eda98c852acfca36e4db3e000cd086
ssides <steven.r.sides@gmail.com>
2022-12-27 21:21:57 -0500

    Turned off Game Controller debug logs.

--- config/config.php
+++ config/config.php
@@ -10,7 +10,7 @@
   $cardImageHeight = 70; // px
   $firstJackChoices = 500;
   $dealChoices = 10000;
-  $gameControllerLog = true;
+  $gameControllerLog = false;
   $version = '1.1:3'; // displayed in the header. change in version forces the browser to get the latest styles.
                       // M.m:s   M - Major version number, m - minor version number, s - styles version number.
 ?>
