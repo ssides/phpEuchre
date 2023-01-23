f4b42ae126570ee6534c21d6cbe0ee29e885e49e
ssides <steven.r.sides@gmail.com>
2023-01-23 15:13:49 -0500

    Made the debug log conditional.

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
   // make sure there is a final path separator in paths.
-  $appUrl = 'http://192.168.254.148:8080/';
+  $appUrl = 'http://localhost:8080/';
   $cookieName = 'sidesLocalEuchre_cookie';
   $thumbnailDim = 25;
   $positionDistance = 3;
@@ -11,7 +11,7 @@
   $firstJackChoices = 500;
   $dealChoices = 12000;
   $gameControllerLog = false;
-  $delayClearTable = 2;
+  $clearTableDelay = 2;
   $version = '1.3:3'; // displayed in the header. change in version forces the browser to get the latest styles.
                       // M.m:s   M - Major version number, m - minor version number, s - styles version number.
 ?>
