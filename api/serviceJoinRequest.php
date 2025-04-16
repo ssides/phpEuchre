0c54829c7f194aa52db1669fb017483b23bd26de
ssides <steven.r.sides@gmail.com>
2025-04-16 06:38:30 -0400

    Update GroupRequest when manager accepts or denies.

--- api/serviceJoinRequest.php
+++ api/serviceJoinRequest.php
@@ -6,7 +6,7 @@
 
   if ($_SERVER["REQUEST_METHOD"] !== 'POST') {
     http_response_code(405); // Method Not Allowed
-    echo 'Expecting request method: POST'];
+    echo 'Expecting request method: POST';
     exit;
   }
 
@@ -20,7 +20,7 @@
     || empty($_POST['code'])
   ) {
     http_response_code(400); // Bad Request
-    echo 'One or more request parameters are missing or invalid'];
+    echo 'One or more request parameters are missing or invalid';
     exit;
   }
 
@@ -38,7 +38,18 @@
     exit;
   }
 
+  mysqli_begin_transaction($conn);
+
   try {
+    // Update GroupRequest
+    $stmt = mysqli_prepare($conn, "update `GroupRequest` set `IsActive` = ? where `PlayerID` = ? and `GroupID` = ?");
+    if (!$stmt) { throw new Exception(mysqli_error($conn)); }
+    
+    mysqli_stmt_bind_param($stmt, 'sss', $code, $pid, $gid);
+    if (!mysqli_stmt_execute($stmt)) { throw new Exception(mysqli_stmt_error($stmt)); }
+    mysqli_stmt_close($stmt);
+
+    // Insert into PlayerGroup if code is 'A'
     if ($code === 'A') {
       $stmt = mysqli_prepare($conn, 
           "insert into `PlayerGroup` (id, playerid, groupid, isactive, insertdate) 
@@ -51,10 +62,12 @@
       mysqli_stmt_close($stmt);
     }
 
+    mysqli_commit($conn);
     http_response_code(200);
     echo "";
 
   } catch (Exception $e) {
+    mysqli_rollback($conn);
     trigger_error($e->getMessage() . "\nStack trace: " . $e->getTraceAsString(), E_USER_ERROR);
     http_response_code(500);
     echo "Internal server error.";
